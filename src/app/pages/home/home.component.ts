5405e912117ec5e93052a1f814863153fa4a7f79
anej.bradac <brad.vran.anej@gmail.com>
2024-08-03 20:59:33 +0200

    prep to display notes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { OrderListModule } from 'primeng/orderlist';
+import { Note } from '../../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [OrderListModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  notes: Note[] = [{ title: 'Dada' }, { title: 'Mama' }];
 }
